86518fbada996857e44232211e99731d81faa4df
Noel Kwan <noelkwan1998@gmail.com>
2024-03-23 18:56:37 +0800

    fix term, cmd key

--- xmonad.hs
+++ xmonad.hs
@@ -20,7 +20,7 @@ main = do
             -- spawnAndDo (doShift "Prog2") "emacsclient -c -a \"\""
             -- spawnAndDo (doShift "Term") "emacsclient -c -a \"\""
            -- spawnAndDo (doShift "Spotify") "spotify"
-          terminal = "konsole"
+          terminal = "alacritty"
         , workspaces = ["Firefox", "Chromium", "Prog1", "Prog2", "Scratch", "Notes", "term1", "term2", "School1", "School2"]
         , manageHook = manageSpawn <+> manageDocks <+> manageHook def
         , layoutHook = avoidStruts  $  layoutHook def
@@ -37,7 +37,7 @@ main = do
         -- There could be hardware issue + xmonad issue.
         -- e.g. if hardware key is unresponsive,
         -- xmonad ignores the presses from it after some time.
-        -- , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
         -- additionally seems like superL works after alt is pressed.
         -- regression happens overtime, e.g. maybe 4-5 times after pc is rebooted and xmonad is spawned.
         -- start xmonad:
@@ -45,20 +45,20 @@ main = do
         } `additionalKeys`
         [ ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s -e \'mv $f ~/Pictures/screenshot.jpg\'")
         , ((0, xK_Print), spawn "scrot -e \'mv $f ~/Pictures/screenshot.jpg\'")
-        , ((mod1Mask, xK_m), safeSpawnProg "dmenu_run") -- remap from defaults
-        , ((mod1Mask, xK_d), safeSpawnProg "emacs")
-        , ((mod1Mask, xK_s), spawn "source /home/noel/user-utils/screen-multi-2.sh")
-        , ((mod1Mask, xK_a), spawn "source /home/noel/user-utils/screen-single.sh")
+        , ((mod4Mask, xK_m), safeSpawnProg "dmenu_run") -- remap from defaults
+        , ((mod4Mask, xK_d), safeSpawnProg "emacs")
+        , ((mod4Mask, xK_s), spawn "source /home/noel/user-utils/screen-multi-2.sh")
+        , ((mod4Mask, xK_a), spawn "source /home/noel/user-utils/screen-single.sh")
         -- , ((mod4Mask, xK_i), safeSpawnProg "source /home/noel/user-utils/intellij.sh")
         -- Workaround for screen ordering
-        , ((mod1Mask, xK_w), viewScreen horizontalScreenOrderer (P 0))
-        , ((mod1Mask, xK_e), viewScreen horizontalScreenOrderer (P 1))
-        , ((mod1Mask, xK_r), viewScreen horizontalScreenOrderer (P 2))
-        , ((mod1Mask, xK_F9), setMute True >> lowerVolume 4 >> return ())
-        , ((mod1Mask, xK_F10), setMute True >> raiseVolume 4 >> return ())
-        , ((mod1Mask, xK_F8), toggleMute >> return ())
-        , ((mod1Mask, xK_F6), spawn "light -U 15")
-        , ((mod1Mask, xK_F7), spawn "light -A 15")
+        , ((mod4Mask, xK_w), viewScreen horizontalScreenOrderer (P 0))
+        , ((mod4Mask, xK_e), viewScreen horizontalScreenOrderer (P 1))
+        , ((mod4Mask, xK_r), viewScreen horizontalScreenOrderer (P 2))
+        , ((mod4Mask, xK_F9), setMute True >> lowerVolume 4 >> return ())
+        , ((mod4Mask, xK_F10), setMute True >> raiseVolume 4 >> return ())
+        , ((mod4Mask, xK_F8), toggleMute >> return ())
+        , ((mod4Mask, xK_F6), spawn "light -U 15")
+        , ((mod4Mask, xK_F7), spawn "light -A 15")
         ]
 
 -- unresponsive xmonad
